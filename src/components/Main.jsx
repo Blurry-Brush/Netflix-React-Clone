498bd154d6fa002cbabf1630827f7baf395c795d
Yuvraj Singh <yuvrajsingh1704@gmail.com>
2022-10-21 00:20:14 +0530

    add image slider animations

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,45 +1,86 @@
-import axios from 'axios';
-import {useEffect, useState} from 'react'
-import Requests from '../Requests';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Requests from "../Requests";
+import { textVariant, imageVariant } from "../variants";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Main = () => {
-    const [movies,setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [index, setIndex] = useState(0);
+  // const movie = movies[Math.floor(Math.random()* movies.length)]
+  const movie = movies[index];
+  useEffect(() => {
+    axios.get(Requests.requestPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
+  // console.log(movie);
 
-    const movie = movies[Math.floor(Math.random()* movies.length)]
-    useEffect(()=> {
-        axios.get(Requests.requestPopular).then((response) => {
-            setMovies(response.data.results);
-        })
-    },[])
-    // console.log(movie);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % movies.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  });
 
-    const truncateString = (str,num) => {
-        if(str?.length > num){
-            return str.slice(0,num) + "...";
-        }
-        else{
-            return str;
-        }
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
     }
+  };
 
   return (
-    <div className='w-full h-[600px] text-white'>
-        <div className='w-full h-full'>
-            <div className='w-full h-[600px] absolute bg-gradient-to-r from-black'></div>
-            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}/>
-        </div>
-        <div className='absolute p-4 md:p-8 w-full top-[20%]'>
-            <h1 className='text-3xl lg:text-5xl font-bold'>{movie?.title}</h1>
-            <div className='my-4'>
-                <button className='bg-gray-300 hover:bg-transparent hover:text-white transition border border-gray-300 text-black py-2 px-5'>Play</button>
-                <button className='border border-gray-300 py-2 px-5 ml-4 transition-colors hover:text-black  hover:bg-gray-300'>Watch Later</button>
-            </div>
-            <p className='text-gray-400 text-sm'> Released: {movie?.release_date}</p>
-            <p className='w-full md:max-w-xl lg:max-w-lg xl:max-w-md'>{truncateString(movie?.overview,150)}</p>
-        </div>
+    <AnimatePresence>
+      <motion.div
+        key={index}
+        variants={imageVariant}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="w-full h-[600px] text-white"
+      >
+        <motion.div className="w-full h-full">
+          <motion.div className="w-full h-[600px] absolute bg-gradient-to-r from-black"></motion.div>
+          <img
+            className="w-full h-full object-cover"
+            src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+        </motion.div>
+        <AnimatePresence>
+          <motion.div
+            key={index}
+            variants={textVariant}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="absolute p-4 md:p-8 w-full top-[20%]"
+          >
+            <motion.h1 className="text-3xl lg:text-5xl font-bold">
+              {movie?.title}
+            </motion.h1>
+            <motion.div className="my-4">
+              <button className="bg-gray-300 hover:bg-transparent hover:text-white transition border border-gray-300 text-black py-2 px-5">
+                Play
+              </button>
+              <button className="border border-gray-300 py-2 px-5 ml-4 transition-colors hover:text-black  hover:bg-gray-300">
+                Watch Later
+              </button>
+            </motion.div>
+            <motion.p className="text-gray-400 text-sm">
+              {" "}
+              Released: {movie?.release_date}
+            </motion.p>
+            <motion.p className="w-full md:max-w-xl lg:max-w-lg xl:max-w-md">
+              {truncateString(movie?.overview, 150)}
+            </motion.p>
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Main
+export default Main;
---
+++ src/variants.js
@@ -0,0 +1,38 @@
+const textVariant= {
+    initial: {
+        x:-60,
+        opacity: 0
+    },
+    animate: {
+        x:0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            ease: "easeInOut",
+            
+        }
+    },
+    exit: {
+        x:-60,
+        opacity: 0,
+    }
+}
+
+const imageVariant =  {
+    initial: {
+        opacity: 0
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            duration: 1,
+            ease: "easeInOut",
+        }
+    },
+    exit: {
+        opacity: 0
+    }
+
+}
+
+export {textVariant, imageVariant}
